b46638750af515de5ac85054f95117e677180fb8
Beverly Joseph <berverlyjoseph3@gmail.com>
2026-02-15 00:34:54 +0300

    Update MegaMenu.tsx

--- components/layout/MegaMenu.tsx
+++ components/layout/MegaMenu.tsx
@@ -9,13 +9,13 @@ export const MegaMenu = () => {
       <div className="absolute left-0 top-full pt-4 hidden group-hover:block">
         <div className="bg-white rounded-lg shadow-xl p-6 w-96">
           <div className="grid grid-cols-2 gap-4">
-            <Link href="/category/mobility" className="hover:text-medical-blue">
+            <Link href="/category/mobility-aids" className="hover:text-medical-blue">
               Mobility Aids
             </Link>
             <Link href="/category/respiratory" className="hover:text-medical-blue">
               Respiratory
             </Link>
-            <Link href="/category/furniture" className="hover:text-medical-blue">
+            <Link href="/category/hospital-furniture" className="hover:text-medical-blue">
               Hospital Furniture
             </Link>
             <Link href="/category/diagnostic" className="hover:text-medical-blue">
