1cb4ca176e69f8250d82ee76ffd6a6c0547e787e
Beverly Joseph <berverlyjoseph3@gmail.com>
2026-02-14 17:13:24 +0300

    Create index.ts

---
+++ types/index.ts
@@ -0,0 +1,30 @@
+export interface Product {
+  id: number;
+  name: string;
+  slug: string;
+  sku: string;
+  price: number;
+  salePrice?: number;
+  image: string;
+  category: string;
+  categorySlug: string;
+  brand: string;
+  rating: number;
+  reviewCount: number;
+  inventory: number;
+  description: string;
+  deliveryEstimate: string;
+  warranty: string;
+  features?: string[];
+  specifications?: Array<{ name: string; value: string }>;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+  slug: string;
+  image: string;
+  description: string;
+  icon: string;
+  productCount: number;
+}
